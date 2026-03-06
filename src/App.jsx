1975a40f2529d5347c1b37e9dfdf42ca64542b4d
ega394 <ega394@users.noreply.github.com>
2026-03-06 09:09:38 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import JadwalPimpinan from './jadwal-pimpinan'
+const [events, setEvents] = useState([]);
+const [dbReady, setDbReady] = useState(false);
 
-function App() {
-  return <JadwalPimpinan />
-}
+useEffect(() => {
+  loadFromSupabase()
+    .then(data => {
+      setEvents(data.length > 0 ? data : seed);
+      setDbReady(true);
+    })
+    .catch(() => { setEvents(seed); setDbReady(true); });
+}, []);
 
-export default App
+// Auto-save setiap kali events berubah
+useEffect(() => {
+  if (dbReady) saveToSupabase(events);
+}, [events]);
